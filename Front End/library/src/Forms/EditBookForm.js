b3067a5bfb9e7001f4170add372f3b0c87985ab6
Daniel <dx555r@gmail.com>
2018-12-01 19:22:52 +0400

    Fixed edit form input to be changable

--- Front End/library/src/Forms/EditBookForm.js
+++ Front End/library/src/Forms/EditBookForm.js
@@ -34,13 +34,13 @@ export class EditBookForm extends React.PureComponent {
  return (   
     <form onSubmit={ (e) => {e.preventDefault(); this.props.handleFormEdit(this.state.val, formFields.name.value, formFields.description.value, formFields.rating.value, formFields.author_id.value); e.target.reset() }}>
     <p>Edit Book</p>
-    <input ref={input => formFields.name = input} value={books.name} type="text"></input>
+    <input ref={input => formFields.name = input} defaultValue={books.name} type="text"></input>
     <br></br>
-    <input ref={input => formFields.description = input} value={books.description} type="text"></input>
+    <input ref={input => formFields.description = input} defaultValue={books.description} type="text"></input>
     <br></br>
-    <input ref={input => formFields.rating = input} value={books.rating} type="number"></input>
+    <input ref={input => formFields.rating = input} defaultValue={books.rating} type="number"></input>
     <br></br>
-    <input ref={input => formFields.author_id = input} value={books.author_id} type="number"></input>
+    <input ref={input => formFields.author_id = input} defaultValue={books.author_id} type="number"></input>
     <br></br>
     <button>Submit Form</button>
     </form>
